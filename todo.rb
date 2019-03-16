9e0a79411c8245d55fe0f11a4a2626753fe58ee1
Ismael Sane <six.echo.sa@gmail.com>
2019-03-16 17:54:03 +0100

    Add as a user, I can perform actions on the Todo List through a menu.

--- todo.rb
+++ todo.rb
@@ -1,3 +1,23 @@
+module Menu
+  def menu
+    " Welcome to the TodoList Program!
+    Please press the key 1 if you want to ADD a list.
+    Please press the key 2 if you want to SHOW a list.
+    Please press the key Q if you want to QUIT the program"
+  end
+  def show
+    menu
+  end
+end
+
+module Promptable
+  def prompt(message='What would you like to do?', symbol=':> ')
+    print message
+    print symbol
+    gets.chomp
+  end
+end
+
 class List
 
   attr_reader :description, :all_tasks
@@ -23,11 +43,21 @@ class Task
 end
 
 if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
-  puts 'You have created a new list'
-  puts "What's the name of the task?"
-  task = gets.chomp
-  my_list.add(Task.new(task))
-  puts "You have added #{task} to the Todo List"
-  puts my_list.show
+  include Menu
+  include Promptable
+  actual_list = List.new
+  puts 'Please choose from the following list'
+  until ['q'].include?(user_input = prompt(show).downcase)
+    case user_input
+    when '1'
+      actual_list.add(Task.new(prompt('What is the task you would
+      like to accomplish?')))
+      puts "You have added a new task to the Todo List"
+    when '2'
+      puts actual_list.show
+    else
+      puts "Sorry, I did not understand"
+    end
+  end
+  puts "Outro - Thanks for using the menu system!"
 end
