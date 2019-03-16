c5977e64eabf7245080d4051fa96fac64c01fcb4
Ismael Sane <six.echo.sa@gmail.com>
2019-03-16 16:07:33 +0100

    Add as a user I can see all the tasks in a list

--- todo.rb
+++ todo.rb
@@ -9,6 +9,10 @@ class List
   def add(task)
     all_tasks << task
   end
+
+  def show
+    all_tasks.to_s
+  end
 end
 
 class Task
@@ -21,8 +25,9 @@ end
 if __FILE__ == $PROGRAM_NAME
   my_list = List.new
   puts 'You have created a new list'
-  puts "what's the name of the task?"
+  puts "What's the name of the task?"
   task = gets.chomp
   my_list.add(Task.new(task))
   puts "You have added #{task} to the Todo List"
+  puts my_list.show
 end
